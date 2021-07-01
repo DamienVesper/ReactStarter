af4fc86e5d013ba1bb3ab2a28461709d87a78b21
Damien Vesper <ldamienvesper@gmail.com>
2021-07-01 13:38:20 -0500

    fix eslint

--- webpack/webpack.prod.ts
+++ webpack/webpack.prod.ts
@@ -2,8 +2,21 @@ import merge from 'webpack-merge';
 
 import common from './webpack.common';
 
+import * as path from 'path';
+
 const config = merge(common, {
-    mode: `production`
+    mode: `production`,
+
+    output: {
+        filename: `[name].bundle.js`,
+        path: path.resolve(__dirname, `dist`)
+    },
+
+    optimization: {
+        splitChunks: {
+            chunks: `all`
+        }
+    }
 });
 
 export default config;
